123f0a548cf38f783e7451eabbc70003826679b5
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-11 05:23:53 -0300

    Feat: change logo

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -27,11 +27,11 @@ export const links = [
     items: [
       {
         label: "Exchange",
-        href: "https://exchange.pancakeswap.finance",
+        href: "https://exchange.candyswap.online",
       },
       {
         label: "Liquidity",
-        href: "https://exchange.pancakeswap.finance/#/pool",
+        href: "https://exchange.candyswap.online/#/pool",
       },
     ],
   },
