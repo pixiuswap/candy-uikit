1ff8c27967b88b04742eca9394076699640a680c
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-28 12:53:00 -0300

    Feat: update sidebar width

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -207,5 +207,5 @@ export const socials = [
 
 export const MENU_HEIGHT = 64;
 export const MENU_ENTRY_HEIGHT = 48;
-export const SIDEBAR_WIDTH_FULL = 275;
+export const SIDEBAR_WIDTH_FULL = 300;
 export const SIDEBAR_WIDTH_REDUCED = 56;
